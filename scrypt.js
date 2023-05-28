b7bd9edbd60ef449ee6c62f705ba606ee0a2f57c
leonardopea <leonardo.pea.1996@gmail.com>
2023-05-28 21:51:48 +0200

    esercizio completo con bonus 2 svolto

--- scrypt.js
+++ scrypt.js
@@ -4,18 +4,23 @@ let numeri = document.getElementById('numeri');
 
 for ( let x =1; x<=100; x++){
 
-    let y =document.createElement('div');
+    let y = document.createElement('div');
+    y.classList.add('quadrato');
 
     if((x % 5 === 0 ) && (x % 3 === 0)){
         y.innerText="FizzBuzz";
+        y.classList.add('fizzbuzz');
     }
 
     else if (x % 3 === 0){
         y.innerText="Fizz";
+        y.classList.add('fizz');
     }
 
     else if ( x % 5 === 0){
         y.innerText="Buzz";
+        y.classList.add('buzz');
+        
     }
     else{
         y.innerText= x ;
